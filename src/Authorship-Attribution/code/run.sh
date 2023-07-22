ba3f2616972dea776d211168b5e90ea05d549ec2
rebibabo <2841329980@qq.com>
2023-07-22 11:05:35 +0800

    修改run.sh

--- src/Authorship-Attribution/code/run.sh
+++ src/Authorship-Attribution/code/run.sh
@@ -1,15 +1,15 @@
 CUDA_VISIBLE_DEVICES=0 python run.py \
-    --output_dir=./saved_models/ProgramData \
+    --output_dir=./saved_models/gcjpy \
     --model_type=roberta \
     --config_name=microsoft/codebert-base \
     --model_name_or_path=microsoft/codebert-base \
     --tokenizer_name=roberta-base \
-    --number_labels 105 \
+    --number_labels 65 \
     --do_train \
     --calc_asr \
-    --train_data_file=../dataset/data_folder/ProgramData/one-style/19/processed_data/train_pert.jsonl \
-    --eval_data_file=../dataset/data_folder/ProgramData/one-style/19/processed_data/test_pert.jsonl \
-    --epoch 3 \
+    --train_data_file=../dataset/data_folder/author_file/tokensub/train_pert.jsonl \
+    --eval_data_file=../dataset/data_folder/author_file/tokensub/test_pert.jsonl \
+    --epoch 20 \
     --block_size 512 \
     --train_batch_size 16 \
     --eval_batch_size 512 \
@@ -18,8 +18,3 @@ CUDA_VISIBLE_DEVICES=0 python run.py \
     --evaluate_during_training \
     --saved_model_name=style_pert_oj \
     --seed 123456 2>&1| tee train_gcjpy.log \
-
-
-
-#--eval_data_file=../dataset/data_folder/ProgramData/one-style/array-to-pointer/processed_test/test.csv \
-#--eval_data_file=../dataset/data_folder/ProgramData/processed_test_origin/test.csv \
